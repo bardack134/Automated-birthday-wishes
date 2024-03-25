ab8e98f115e74f0cba99998929e31eb6f6ff4e8d
Ricardo Antonio Gomez Villalobos <bardack134@gmail.com>
2024-03-25 14:45:20 +0900

    correo enviado, con el msg personalizado

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Automated-birthday-wishes
-Automated Birthday Wishes is a Python application that allows you to automatically send birthday messages via email using the smtplib module. Customize your messages, schedule the sending date, and let the application take care of the rest!
+This repository contains a Python script that automatically sends a personalized birthday email to friends listed in a CSV file. The script checks if today’s date matches any birthday in the CSV file. If it does, the script selects a random letter template, personalizes it with the friend’s name, and sends the email.
+
+Tools Used:
+
+Python: The script is written in Python.
+pandas: Used to read the CSV file containing the birthdays.
+datetime: Used to get the current date.
+smtplib: Used to send the email via Gmail’s SMTP server.
+email: Used to create the email message.
+random: Used to select a random letter template.
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import datetime
+from email import encoders
+from email.mime.base import MIMEBase
 import random 
 from pandas import *
 import smtplib
@@ -6,26 +8,6 @@ from constants import *
 
 my_email=EMAIL
 password=PASSWORD
-message="subject:Hello followers\n\nexample using constans"
-
-
-# #creamos un objeto de este modulo que recibe 2 parametros, mail-server y puerto
-# with  smtplib.SMTP("smtp.gmail.com", 587) as connection:
-
-
-#     #  TLS (Transport Layer Security), usamos esta funcion para encriptar nuestro msj y para una conexion segura
-#     connection.starttls()
-
-
-#     #ingresamos nuestro correo y contrasena a la funcion login
-#     connection.login(user=my_email, password=password)
-
-
-#     # enviamos nuestro correo
-#     connection.sendmail(from_addr=my_email,
-#                         to_addrs="bardack134@gmail.com", 
-#                         msg=message)
-
 
 
 # # NOTE:logre enviar el correo, intentar enviar el correo pero creando constantes
@@ -52,68 +34,117 @@ today_day=today_date.day
 for dict in birthday_dict:
     
     #compruebo si la fecha de hoy coincide con algun cumplea;os
-    if today_month == dict['month']:
+    if today_month == dict['month'] and today_day==dict["day"]:
+        
         
         friend_name=dict['name']
         
+        
         friend_email=dict['email']
-  
         
-#lista de cartas, aca pondremos todas nuestras cartas
-letter_list=[]
-
-#TODO: ESCOGEMOS UN MENSAJE ALEATORIO DE NUESTROS MENSAJES    
-with open("letter_templates/letter_1.txt", "r")  as letter1:
-    
-    #creamos una lista de lineas del archivo
-    letter_1=letter1.readlines()
-    
-    
-    #agregamos nuestra carta a la lista de cartas
-    letter_list.append(letter_1)
-
-
-#hacemos  el mismo procedimiento con las demas cartas
-with open("letter_templates/letter_2.txt", "r")  as letter2:
-    
-    
-    letter_2=letter2.readlines()
         
-    
-    letter_list.append(letter_2)
-    
-    
-with open("letter_templates/letter_3.txt", "r")  as letter3:
-    
-    
-    letter_3=letter3.readlines()
+        send_email_to=friend_email
         
-    
-    letter_list.append(letter_3)
-    
+        #lista de cartas, aca pondremos todas nuestras cartas
+        letter_list=[]
 
-#escogemos aleatoriamente una de nuetras letter de la lista de letters
-random_letter=random.choice(letter_list)
-print(random_letter)
-
-
-#obtenemos el lugar donde esta escrito [NAME]
-initial_msg=random_letter[0]
-print(initial_msg)
-
-
-#incrustamos el　nombre de nuestro amigo en [NAME]    
-new_initial_msg=initial_msg.replace("[NAME]", friend_name)
-print(new_initial_msg)
+        #TODO: ESCOGEMOS UN MENSAJE ALEATORIO DE NUESTROS MENSAJES    
+        with open("letter_templates/letter_1.txt", "r")  as letter1:
+            
+            #creamos una lista de lineas del archivo
+            letter_1=letter1.readlines()
+            
+            
+            #agregamos nuestra carta a la lista de cartas
+            letter_list.append(letter_1)
 
 
-#remplazamos el inicio del mensaje con el nuevo mensaje en random_letter
-random_letter[0]=new_initial_msg
-print(random_letter)
+        #hacemos  el mismo procedimiento con las demas cartas
+        with open("letter_templates/letter_2.txt", "r")  as letter2:
+            
+            
+            letter_2=letter2.readlines()
+                
+            
+            letter_list.append(letter_2)
+            
+            
+        with open("letter_templates/letter_3.txt", "r")  as letter3:
+            
+            
+            letter_3=letter3.readlines()
+                
+            
+            letter_list.append(letter_3)
+            
 
-#NOTE: TENEMOS EN FORMATO LISTA, LA CARTA PERSONALIZADA CON EL NOMBRE DE NUESTRO AMIGO, ...APARTIR DE AHORA CREAR LA CARTA EN FORMATO .TXT
+        #escogemos aleatoriamente una de nuetras letter de la lista de letters
+        random_letter=random.choice(letter_list)
+        print(random_letter)
 
-# 4. Send the letter generated in step 3 to that person's email address.
+
+        #obtenemos el lugar donde esta escrito [NAME]
+        initial_msg=random_letter[0]
+        print(initial_msg)
+
+
+        #incrustamos el　nombre de nuestro amigo en [NAME]    
+        new_initial_msg=initial_msg.replace("[NAME]", friend_name)
+        print(new_initial_msg)
+
+
+        #remplazamos el inicio del mensaje con el nuevo mensaje en random_letter
+        random_letter[0]=new_initial_msg
+        print(random_letter)
+
+
+        #NOTE: TENEMOS EN FORMATO LISTA, LA CARTA PERSONALIZADA CON EL NOMBRE DE NUESTRO AMIGO, ...APARTIR DE AHORA CREAR LA CARTA EN FORMATO .TXT
+        with open("happy_birthday_letter.txt", "w") as happy_birthday_letter:
+            
+            happy_birthday_letter.writelines(random_letter)
+            
+            
+        with open("happy_birthday_letter.txt", "r") as happy_birthday_letter:
+            
+            #leemos la carta y la guardamos en una variable, esta variable tiene el mensaje a ser enviado por el correo
+            msg=happy_birthday_letter.read()
+
+           
+        # creamos un objeto de este modulo que recibe 2 parametros, mail-server y puerto
+        with  smtplib.SMTP("smtp.gmail.com", 587) as connection:
+            
+            #TLS (Transport Layer Security), usamos esta funcion para encriptar nuestro msj y para una conexion segura
+            connection.starttls()            
+            
+
+            #ingresamos nuestro correo y contrasena a la funcion login
+            connection.login(user=my_email, password=password)       
+            
+            
+            subject='Happy birthday'
+                               
+            
+            #enviamos nuestro correo
+            connection.sendmail(from_addr=my_email, 
+                                to_addrs=send_email_to, 
+                                msg='subject: {} \n\n {}'.format(subject, msg))       
+            
+              
+    else:
+        print("No friend has a birthday today")
+                
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
